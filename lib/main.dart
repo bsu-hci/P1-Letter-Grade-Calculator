d2db8bb25fa8fdd4d78d60fcfe371e8312a64bf2
Collin Stiner <collincollin16@gmail.com>
2020-09-26 20:36:51 -0400

    Add grading logic

--- lib/main.dart
+++ lib/main.dart
@@ -5,25 +5,12 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Grade Calculator',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Grade Calculator'),
@@ -33,16 +20,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -50,51 +27,44 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  TextEditingController pointedEarnedTextField = new TextEditingController();
+  TextEditingController totalPointsTextField = new TextEditingController();
+  TextEditingController letterGradeTextField = new TextEditingController();
+  TextEditingController scoreTextField = new TextEditingController();
 
-  void _incrementCounter() {
+  void _calcGrade() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      double pointsEarned = double.parse(pointedEarnedTextField.text);
+      double totalPoints = double.parse(totalPointsTextField.text);
+      double score = (pointsEarned / totalPoints) * 100;
+      String letterGrade = getLetterGrade(score);
+      letterGradeTextField.text = letterGrade;
+      scoreTextField.text = score.toStringAsFixed(1);
     });
+  }
+
+  String getLetterGrade(double score) {
+    if (score > 89.0) {
+      return "A";
+    } else if (score > 79.0) {
+      return "B";
+    } else if (score > 69.0) {
+      return "C";
+    } else if (score > 59.0) {
+      return "D";
+    } else {
+      return "F";
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text('Enter Score:', style: TextStyle(fontSize: 20)),
@@ -105,8 +75,8 @@ class _MyHomePageState extends State<MyHomePage> {
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
                   new Flexible(
-                    child: new TextField(
-                      obscureText: false,
+                    child: TextFormField(
+                      controller: pointedEarnedTextField,
                       decoration: InputDecoration(
                         border: OutlineInputBorder(),
                         labelText: 'Points Earned',
@@ -117,8 +87,8 @@ class _MyHomePageState extends State<MyHomePage> {
                   Text('/', style: TextStyle(fontSize: 30)),
                   const SizedBox(width: 20.0),
                   new Flexible(
-                    child: new TextField(
-                      obscureText: false,
+                    child: TextFormField(
+                      controller: totalPointsTextField,
                       decoration: InputDecoration(
                         border: OutlineInputBorder(),
                         labelText: 'Total Points',
@@ -130,7 +100,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             const SizedBox(height: 30),
             RaisedButton(
-              onPressed: _incrementCounter,
+              onPressed: _calcGrade,
               child: const Text('Submit', style: TextStyle(fontSize: 20)),
             ),
             const SizedBox(height: 50),
@@ -142,9 +112,9 @@ class _MyHomePageState extends State<MyHomePage> {
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
                   new Flexible(
-                    child: new TextField(
+                    child: TextField(
+                      controller: letterGradeTextField,
                       readOnly: true,
-                      obscureText: false,
                       decoration: InputDecoration(
                         border: OutlineInputBorder(),
                         labelText: 'Grade',
@@ -163,9 +133,9 @@ class _MyHomePageState extends State<MyHomePage> {
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
                   new Flexible(
-                    child: new TextField(
+                    child: TextField(
+                      controller: scoreTextField,
                       readOnly: true,
-                      obscureText: false,
                       decoration: InputDecoration(
                         border: OutlineInputBorder(),
                         labelText: 'Score (%)',
@@ -178,7 +148,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
